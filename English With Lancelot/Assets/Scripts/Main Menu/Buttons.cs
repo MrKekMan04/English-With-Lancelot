73cb18c26ae5b3b621e8a79f4ac366c30439a88d
Vitaliy <vitaliy-efimov111@mail.ru>
2023-03-26 19:18:47 +0500

    Refactor

--- English With Lancelot/Assets/Scripts/Main Menu/Buttons.cs
+++ English With Lancelot/Assets/Scripts/Main Menu/Buttons.cs
@@ -4,5 +4,6 @@ using UnityEngine.SceneManagement;
 public class Buttons : MonoBehaviour
 {
     public void StartGame() => SceneManager.LoadScene("Game");
+    
     public void QuitGame() => Application.Quit();
 }
